facc3c94fd00a71e2f47599e9b85afa345793863
Yunseo Kim <git@yunseo.kim>
2025-10-13 19:41:49 +0900

    feat: add some hardened settings
    - Disable referrer and storage access for resources injected by content scripts. [FF139+]
    - Enable letterboxing [FF67+]

--- user-overrides.js
+++ user-overrides.js
@@ -47,6 +47,7 @@ user_pref("browser.link.force_default_user_context_id_for_external_opens", true)
 user_pref("media.peerconnection.ice.no_host", true); // [SETUP-HARDEN] 2004: force exclusion of private IPs from ICE candidates [FF51+]. This will protect your private IP even in TRUSTED scenarios after you grant device access, but often results in breakage on video-conferencing platforms
 
 user_pref("permissions.default.shortcuts", 2); // 2615: disable websites overriding Firefox's keyboard shortcuts [FF58+]
+user_pref("privacy.antitracking.isolateContentScriptResources", true); // 2635: disable referrer and storage access for resources injected by content scripts [FF139+]
 
 /* 2660: limit allowed extension directories
  * 1=profile, 2=user, 4=application, 8=system, 16=temporary, 31=all
@@ -59,7 +60,32 @@ user_pref("extensions.enabledScopes", 7); // [HIDDEN PREF]
 
 user_pref("extensions.webextensions.restrictedDomains", ""); // 2662: disable webextension restrictions on certain mozilla domains (you also need 4503) [FF60+]
 
-user_pref("privacy.resistFingerprinting", true); // 4501: enable RFP
+/* 4501: enable RFP
+ * [NOTE] pbmode applies if true and the original pref is false
+ * [SETUP-WEB] RFP can cause some website breakage: mainly canvas, use a canvas site exception via the urlbar.
+ * RFP also has a few side effects: mainly that timezone is GMT, and websites will prefer light theme ***/
+// Hardened settings to enable RFP.
+user_pref("privacy.resistFingerprinting", true); // [FF41+]
+user_pref("privacy.resistFingerprinting.pbmode", true); // [FF114+]
+
+/* 4504: enable letterboxing [FF67+]
+ * Dynamically resizes the inner window by applying margins in stepped ranges [2]
+ * If you use the dimension pref, then it will only apply those resolutions.
+ * The format is "width1xheight1, width2xheight2, ..." (e.g. "800x600, 1000x1000")
+ * [SETUP-WEB] This is independent of RFP (4501). If you're using RFP, but dislike the
+ * margins, then don't enable this pref, keeping in mind that it is effectively fingerprintable
+ * [WARNING] DO NOT USE: the dimension pref is only meant for testing
+ * [1] https://bugzilla.mozilla.org/1407366
+ * [2] https://hg.mozilla.org/mozilla-central/rev/7211cb4f58ff#l5.13 ***/
+// Hardened setting to enable letterboxing.
+user_pref("privacy.resistFingerprinting.letterboxing", true); // [HIDDEN PREF]
+
+/* 4505: disable RFP by domain [FF91+]
+ * [NOTE]: The pref takes comma separated values: e.g. "*domain1.tld, *domain2.tld"
+ * Working domain examples: "arkenfox.github.io", "*github.io"
+ * Non-working domain examples: "https://arkenfox.github.io", "github.io", "*arkenfox.github.io" ***/
+// user_pref("privacy.resistFingerprinting.exemptedDomains", "*.example.invalid"); // Fill in your exceptions here.
+
 user_pref("privacy.spoof_english", 2); // 4506: Enable RFP spoof english (hardened settings) [FF59+] (0=prompt, 1=disabled, 2=enabled)
 
 user_pref("signon.rememberSignons", false); // 5003: disable saving passwords
